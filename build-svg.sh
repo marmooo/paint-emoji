4cb5b23cc13751dbf95dff65e5414b6525c0cc86
marmooo <marmooo@users.noreply.github.com>
2024-05-14 00:05:50 +0900

    update deps

--- build-svg.sh
+++ build-svg.sh
@@ -2,4 +2,4 @@ rsync -a --delete vendor/noto-emoji/svg/ svg/noto-emoji/
 rsync -a --delete vendor/twemoji/assets/svg/ svg/twemoji/
 rsync -a --delete vendor/blobmoji/svg/ svg/blobmoji/
 rsync -a --delete vendor/emojitwo/svg/ svg/emojitwo/
-deno run --allow-read --allow-write build-svg.js
+deno run --allow-read --allow-write --allow-env build-svg.js
